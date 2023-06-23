baa51da6d025b78ae96116ddd9bf57ba52b7b969
Marcin Makarewicz <marcin@makarewicz.eu>
2023-06-23 23:41:31 +0200

    feat(app, real): add reals

--- apps/app/pages/index.tsx
+++ apps/app/pages/index.tsx
@@ -37,8 +37,9 @@ export default function Index() {
       )}
       |<Link href="/auto">auto</Link>|<Link href="/euro">euro</Link>|
       <Link href="/plot">plot</Link>|<Link href="/rate">rate</Link>|
-      <Link href="/ross">ross</Link>|<Link href="/sale">sale</Link>|
-      <Link href="/shot">shot</Link>|<Link href="/xkom">xkom</Link>
+      <Link href="/real">real</Link>|<Link href="/ross">ross</Link>|
+      <Link href="/sale">sale</Link>|<Link href="/shot">shot</Link>|
+      <Link href="/xkom">xkom</Link>
       <Hello />
     </Page>
   );
---
+++ packages/real/src/containers/index.tsx
@@ -0,0 +1,385 @@
+import {
+  type ChangeEventHandler,
+  type ComponentPropsWithoutRef,
+  type Dispatch,
+  type SetStateAction,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import dayjs from "dayjs";
+import { Subject, debounceTime, distinctUntilChanged, map } from "rxjs";
+import { z } from "zod";
+import { LazyImage } from "@acme/components";
+import { DataSchema, ItemSchema } from "../schema";
+
+interface FiltersState {
+  region: string;
+  vendor: string;
+  search: string;
+  limit: number;
+}
+
+interface OptionsState {
+  region: string[];
+  vendor: string[];
+}
+
+const LIMIT = [...Array(10)].map((_value, index) => (index + 1) * 500);
+
+function Link({ href = "#", ...props }: ComponentPropsWithoutRef<"a">) {
+  const hash = href[0] === "#";
+
+  return (
+    <a
+      href={href}
+      target={hash ? undefined : "_blank"}
+      rel={hash ? undefined : "noopener noreferrer"}
+      {...props}
+    />
+  );
+}
+
+function Loading() {
+  return <div>Loading...</div>;
+}
+
+type Data = z.infer<typeof DataSchema>;
+
+type Item = z.infer<typeof ItemSchema>;
+
+// const formatPrice = (price: number) =>
+//   `${new Intl.NumberFormat("pl-PL", {
+//     minimumFractionDigits: 2,
+//   }).format(price)} zł`;
+
+function Gallery({ data }: { data: Data }) {
+  return (
+    <div style={{ width: 120, height: 120, marginRight: "1em" }}>
+      {Object.values(data.main_image)
+        .slice(0, 1)
+        .map((url, key) => url && <LazyImage key={key} src={url} />)}
+    </div>
+  );
+}
+
+function Summary({ data }: { data: Data }) {
+  return (
+    <div>
+      <div style={{ float: "right", fontSize: "small" }}>
+        #
+        <Link
+          href={`#${data.id}`}
+          onClick={(e) => {
+            const range = document.createRange();
+            e.preventDefault();
+            range.selectNode(e.target as HTMLElement);
+            ((selection) =>
+              selection &&
+              (selection.removeAllRanges(), selection.addRange(range)))(
+              window.getSelection()
+            );
+          }}
+        >
+          {data.id}
+        </Link>
+      </div>
+      <strong>{data.name}</strong>
+      {data.address && <i>{` ${data.address}`}</i>}
+      <div
+        style={{
+          fontSize: "small",
+        }}
+      >
+        <div>{data.description}</div>
+        {data.vendor && <div>{data.vendor.name}</div>}
+      </div>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+    </div>
+  );
+}
+
+function Details({
+  data,
+  created,
+  checked,
+}: {
+  data: Data;
+  created: string;
+  checked: string | null;
+}) {
+  return (
+    <div style={{ borderTop: "1px solid lightgray", marginTop: ".25em" }}>
+      <div style={{ float: "right" }}>
+        <small>{dayjs(created).format("MMM D, YYYY H:mm")}</small>
+        {checked && (
+          <small> / {dayjs(checked).format("MMM D, YYYY H:mm")}</small>
+        )}
+      </div>
+      <strong>
+        <span
+          style={{
+            color: "darkslateblue",
+          }}
+        >
+          {`${data.construction_date_range.lower} - ${data.construction_date_range.upper} / ${data.properties}`}
+        </span>
+      </strong>
+    </div>
+  );
+}
+
+function Filters({
+  options,
+  filters,
+  setFilters,
+}: {
+  options: OptionsState;
+  filters: FiltersState;
+  setFilters: Dispatch<SetStateAction<FiltersState>>;
+}) {
+  return (
+    <fieldset>
+      <div>
+        <label>
+          <span>Vendor</span>
+          <select
+            value={filters.vendor}
+            onChange={useCallback<ChangeEventHandler<HTMLSelectElement>>(
+              ({ target }) =>
+                setFilters((filters) => ({
+                  ...filters,
+                  vendor: target.value,
+                })),
+              []
+            )}
+          >
+            {[""].concat(options.vendor).map((value) => (
+              <option key={value} value={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+      <div>
+        <label>
+          <span>Region</span>
+          <select
+            value={filters.region}
+            onChange={useCallback<ChangeEventHandler<HTMLSelectElement>>(
+              ({ target }) =>
+                setFilters((filters) => ({
+                  ...filters,
+                  region: target.value,
+                })),
+              []
+            )}
+          >
+            {[""].concat(options.region).map((value) => (
+              <option key={value} value={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+      <label>
+        <span>Search</span>
+        <input
+          type="search"
+          value={filters.search}
+          onChange={useCallback<ChangeEventHandler<HTMLInputElement>>(
+            ({ target }) =>
+              setFilters((filters) => ({
+                ...filters,
+                search: target.value,
+              })),
+            []
+          )}
+        />
+      </label>
+      <label>
+        <span>Limit</span>
+        <select
+          value={String(filters.limit)}
+          onChange={useCallback<ChangeEventHandler<HTMLSelectElement>>(
+            ({ target }) =>
+              setFilters((filters) => ({
+                ...filters,
+                limit: Number(target.value),
+              })),
+            []
+          )}
+        >
+          {LIMIT.map((value) => (
+            <option key={value} value={String(value)}>
+              {value}
+            </option>
+          ))}
+        </select>
+      </label>
+    </fieldset>
+  );
+}
+
+export function List({ list }: { list: Item[] }) {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div style={{ display: "flex", margin: "1em 0" }}>
+      {list.slice(0, 1).map((item) => (
+        <Gallery key={item.id} data={item.data} />
+      ))}
+      <div style={{ flex: 1 }}>
+        {(show ? list : list.slice(0, 1)).map((item, key) => (
+          <div key={item.id}>
+            {!key && <Summary data={item.data} />}
+            <Details
+              data={item.data}
+              created={item.created}
+              checked={item.checked}
+            />
+            {!show && list.length > 1 && (
+              <div>
+                <a
+                  href="#"
+                  onClick={(e) => (e.preventDefault(), setShow(true))}
+                >
+                  <pre>[...]</pre>
+                </a>
+              </div>
+            )}
+            {/* <pre>{JSON.stringify(item, null, 2)}</pre> */}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export function Price() {
+  const [data, setData] = useState<{ result: Item[] } | null>(null);
+  const [filters, setFilters] = useState<FiltersState>(() => ({
+    region: "",
+    vendor: "",
+    search: "",
+    limit: LIMIT[0],
+  }));
+
+  const [queries, setQueries] = useState(() => filters);
+  const search$ = useMemo(() => new Subject<any>(), []);
+
+  useEffect(() => {
+    const subscription = search$
+      .pipe(
+        map(({ search, ...filters }) =>
+          JSON.stringify({
+            ...queries,
+            ...filters,
+            search: search.toLowerCase().trim(),
+          })
+        ),
+        distinctUntilChanged(),
+        debounceTime(400)
+      )
+      .subscribe((filters) =>
+        setQueries((queries) => ({ ...queries, ...JSON.parse(filters) }))
+      );
+    return () => subscription.unsubscribe();
+  }, [search$]);
+
+  useEffect(() => {
+    search$.next(filters);
+  }, [filters]);
+
+  useEffect(() => {
+    fetch(`/api/real?limit=${filters.limit}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(
+          z
+            .object({
+              result: ItemSchema.array(),
+            })
+            .parse(data)
+        );
+      });
+  }, [filters.limit]);
+
+  const grouped = useMemo(
+    () =>
+      Object.entries(
+        (data ? data.result : [])
+          .sort((a, b) => b.created.localeCompare(a.created))
+          .reduce(
+            (list, item) =>
+              Object.assign(list, {
+                [item.item]: (list[item.item] || []).concat(item),
+              }),
+            {} as Record<string, Item[]>
+          )
+      ).sort((a, b) => b[1][0].created.localeCompare(a[1][0].created)),
+    [data]
+  );
+
+  const filtered = useMemo(
+    () =>
+      grouped.filter(
+        ([id, [{ data }]]) =>
+          (queries.search === "" ||
+            queries.search === id ||
+            data.region.name?.toLowerCase().includes(queries.search) ||
+            data.name?.toLowerCase().includes(queries.search)) &&
+          [data.region.name, ""].includes(queries.region) &&
+          [data.vendor.name, ""].includes(queries.vendor)
+      ),
+    [queries, grouped]
+  );
+
+  const options = useMemo(
+    () =>
+      Object.entries(
+        (data ? data.result : []).reduce(
+          (options, { data }) =>
+            Object.assign(options, {
+              region: Object.assign(options.region || {}, {
+                [data.region.name]: true,
+              }),
+              vendor: Object.assign(options.vendor || {}, {
+                [data.vendor.name]: true,
+              }),
+            }),
+          {} as OptionsState
+        )
+      ).reduce(
+        (options, [key, value]) =>
+          Object.assign(options, {
+            [key]: Object.keys(value).sort(),
+          }),
+        {} as OptionsState
+      ),
+    [data]
+  );
+
+  if (data === null) return <Loading />;
+  console.log({ result: data.result, options, filters, filtered });
+  return (
+    <section>
+      <Filters options={options} filters={filters} setFilters={setFilters} />
+      <small>
+        {filtered.length === grouped.length
+          ? `Showing all of ${grouped.length}`
+          : `Found ${filtered.length} items out of a total of ${grouped.length}`}
+      </small>
+      <ol>
+        {filtered.map(([id, list]) => (
+          <li key={id}>
+            <List list={list} />
+          </li>
+        ))}
+      </ol>
+    </section>
+  );
+}
